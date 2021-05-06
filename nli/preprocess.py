0cbe9f19062ad9acdece511a5cd08a06919357a5
Jun <43443895+hansungj@users.noreply.github.com>
2021-05-07 01:12:54 +0200

    Create preprocess.py

---
+++ nli/preprocess.py
@@ -0,0 +1,19 @@
+import numpy as numpy
+import re
+
+
+
+
+
+def token_to_idx():
+	NotImplementedError
+
+def idx_to_token():
+	NotImplementedError
+
+def encode():
+	NotImplementedError
+
+def decode():
+	NotImplementedError
+
