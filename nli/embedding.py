8d262843c0a5869b78452b8fc96a68c8a4554fab
overnastuhed <58928692+overnastuhed@users.noreply.github.com>
2021-05-14 00:55:25 +0300

    Discussed changes
    Cooccur matrix -> dictionary
    run.py - put everything together

--- nli/embedding.py
+++ nli/embedding.py
@@ -1,17 +1,26 @@
 #embedding.py 
 import numpy as np
 
-def build_coocur_matrix(corp_encoded, token2idx):
-    cooccurence_matrix = np.zeros((len(token2idx), len(token2idx)))
+def build_coocurences(corp_encoded, token2idx, window = 2):
+    cooccurence_dict = dict()
     
     for sent_enc in corp_encoded:
         for i in range(len(sent_enc)):
-            left_edge = max(i-2,0)
-            right_edge = min(i+2,len(sent_enc)-1)
+            if not sent_enc[i] in cooccurence_dict:
+                cooccurence_dict[sent_enc[i]] = dict()
+            left_edge = max(i-window,0)
+            right_edge = min(i+window,len(sent_enc)-1)
+            
             for l in range(left_edge,i):
-                cooccurence_matrix[sent_enc[i]][sent_enc[l]] += 1
+                if sent_enc[l] in cooccurence_dict[sent_enc[i]]:
+                    cooccurence_dict[sent_enc[i]][sent_enc[l]] += 1
+                else:
+                    cooccurence_dict[sent_enc[i]][sent_enc[l]] = 1
             for r in range(i+1,right_edge+1):
-                cooccurence_matrix[sent_enc[i]][sent_enc[r]] += 1
+                if sent_enc[l] in cooccurence_dict[sent_enc[i]]:
+                    cooccurence_dict[sent_enc[i]][sent_enc[l]] += 1
+                else:
+                    cooccurence_dict[sent_enc[i]][sent_enc[l]] = 1
                 
-    return cooccurence_matrix
+    return cooccurence_dict
 
