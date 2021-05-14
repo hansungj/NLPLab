7e77e379340ab06ec54449b06bd3836f6f04ab94
Jun <43443895+hansungj@users.noreply.github.com>
2021-05-14 20:05:44 +0200

    Update utils.py

--- nli/utils.py
+++ nli/utils.py
@@ -29,22 +29,25 @@ def open_json_file(path):
 	raise ValueError('Must give json data path')
 
 
-def open_tsv_file(path):
+def open_tsv_file(path, dic=False):
 
 	if path:
 		with open(path, 'r') as f:
 			data = [l.split('\t') for l in f.read().splitlines()]
 
-		res = defaultdict(list)
-		for line in data:
-			res['story_id'].append(line[0])
-			res['obs1'].append(line[1])
-			res['obs2'].append(line[2])
-			res['hyp1'].append(line[3])
-			res['hyp2'].append(line[4])
-			res['label'].append(line[5])
+		if dic:
+			res = defaultdict(list)
+			for line in data:
+				res['story_id'].append(line[0])
+				res['obs1'].append(line[1])
+				res['obs2'].append(line[2])
+				res['hyp1'].append(line[3])
+				res['hyp2'].append(line[4])
+				res['label'].append(line[5])
 
-		return res
+			return res
+
+		return data
 
 
 	raise ValueError('Must give tsv data path')
