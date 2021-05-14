0cc5f630eba890960d9e761cd0c67631bc82a86c
Jun <43443895+hansungj@users.noreply.github.com>
2021-05-14 20:06:11 +0200

    Update data.py

--- nli/data.py
+++ nli/data.py
@@ -12,22 +12,50 @@ import torch
 from torch.utils.data import DataLoader, Dataset
 
 
+def convert_to_tense(x):
+	NotImplementedError
+
 class AlphaLoader(DataLoader):
 
 	def __init__(self, **kwargs):
-		raise NotImplementedError
+
+		data_type = kwargs.pop('data_type', 'vector')
+		data_path = kwargs.pop('data_path'),
+		vocab = kwargs.pop('vocab'),
+		max_samples = kwargs.pop('max_samples', None),
+		eval_measure = kwargs.pop('eval_measure', 'accuracy')
+		self.dataset = AlphaDataset(
+								data_type,
+								data_path
+								vocab,
+								max_samples,
+								eval_measure
+								)
+
+		if kwargs['mode'] == 'baseline':
+			kwargs['collate_fn'] = baseline_collate_fn
+		super(AlphaLoader, ).__init__(self.dataset, **kwargs)
+
+
+def baseline_collate_fn(x):
+	obs = numpy.array(x[0], dtype=np.int32)
+	hyp1 = numpy.array(x[1], dtype=np.int32)
+	hyp2 = numpy.array(x[2], dtype=np.int32)
+	label = numpy.array(x[3], dtype=np.int32)
+	return obs, hyp1, hyp2, label
 
 class AlphaDataset(Dataset):
 
 	def __init__(self,
-				 tsv,
+				 data_type,
+				 data_path,
 				 vocab,
 				 max_samples,
-				 annotate=False,
 				 eval_measure='accuracy'):
 
 
-		self.tsv_path = tsv
+		self.data_type = data_type
+		self.data_path = data_path
 		self.vocab = vocab
 		self.max_samples = max_samples
 		self.annotate = annotate #annotation mode
@@ -45,9 +73,14 @@ class AlphaDataset(Dataset):
 		else:
 			raise ValueError('unknown evaluation measure')
 
+		#load vocab
+		with open(vocab, 'r') as f:
+			self.vocab = json.loads(f)
 
-		if self.tsv_path is not None:
-			data = open_tsv_file(self.tsv_path)
+
+		#for annotation - load witout tokenizing 
+		if self.data_type == 'tsv': # load directly from unprocessed data
+			data = open_tsv_file(self.data_path)
 
 			self.story_ids = data['story_id']
 			self.obs1 = data['obs1']
@@ -56,26 +89,34 @@ class AlphaDataset(Dataset):
 			self.hyp2 = data['hyp2']
 			self.label = np.array(data['label'], dtype=np.int32)
 
-		if not self.annotate:
-			# convert natural language to tensor through encoding
-			pass
+		# for baseline load without mapping to idx
+		elif self.data_type == 'pickle':
+			with open(self.data_path, 'r') as f:
+				dataset = pickle.load(f)
 
+			self.obs = dataset['obs']
+			self.hyp1 = dataset['hyp1']
+			self.hyp2 = dataset['hyp2']
+			self.label = dataset['label']
 
-		
+		# for vector based models
+		elif data_type == 'h5': # load from processed data
+			NotImplementedError
+
 
 	def __len__(self):
 		if self.max_samples is not None:
 			return self.max_samples
 
-		return len(self.story_ids)
+		return len(self.obs)
 
 	def __getitem__(self, idx):
 
 		if self.annotate:
 			return self.obs1[idx], self.obs2[idx], self.hyp1[idx], self.hyp2[idx], self.label[idx]
 
-		raise NotImplementedError
-
+		if self.data_type == 'pickle':
+			return self.obs1[idx], self.hyp1[idx], self.hyp2[idx], self.label[idx]
 
 	def eval(self):
 
