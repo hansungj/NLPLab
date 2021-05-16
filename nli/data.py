99f504d896f1e4e790d9aae63b434a7d589d8039
Jun <43443895+hansungj@users.noreply.github.com>
2021-05-16 23:37:42 +0200

    Update data.py

--- nli/data.py
+++ nli/data.py
@@ -8,7 +8,6 @@ import json
 import numpy as np
 import pickle
 
-
 from nli.utils import open_tsv_file
 import nli.metrics 
 import torch
@@ -26,17 +25,18 @@ class AlphaLoader(DataLoader):
 		data_path = kwargs.pop('data_path')
 		vocab = kwargs.pop('vocab')
 		max_samples = kwargs.pop('max_samples', None)
-		eval_measure = kwargs.pop('eval_measure', 'accuracy')
 		self.dataset = AlphaDataset(
 								data_type,
 								data_path,
 								vocab,
-								max_samples,
-								eval_measure)
+								max_samples)
 
 		#if kwargs['datatype'] == 'string':
-		if kwargs.pop('mode') == 'baseline':
+		if data_type == 'string':
 			kwargs['collate_fn'] = baseline_collate_fn
+		else:
+			NotImplementedError
+
 		super().__init__(self.dataset, **kwargs)
 
 def baseline_collate_fn(x):
@@ -53,27 +53,12 @@ class AlphaDataset(Dataset):
 				 data_type,
 				 data_path,
 				 vocab,
-				 max_samples,
-				 eval_measure='accuracy'):
-
+				 max_samples):
 
 		self.data_type = data_type
 		self.data_path = data_path
 		self.vocab = vocab
 		self.max_samples = max_samples
-		self.eval_measure = eval_measure
-
-		#evaluation 
-		if eval_measure == 'accuracy':
-			self.eval_function = nli.metrics.accuracy
-		elif eval_measure == 'precision':
-			self.eval_function = nli.metrics.precision
-		elif eval_measure == 'recall':
-			self.eval_function = nli.metrics.recall
-		elif eval_measure == 'f1score':
-			self.eval_function = nli.metrics.f1score
-		else:
-			raise ValueError('unknown evaluation measure')
 
 		#load vocab
 		with open(vocab, 'r') as f:
@@ -118,10 +103,4 @@ class AlphaDataset(Dataset):
 
 		if self.data_type == 'pickle':
 			return self.obs[idx], self.hyp1[idx], self.hyp2[idx], self.label[idx]
-
-	def eval(self):
-
-		if self.label is None or self.pred is None:
-			return None
-		self.eval_result = self.eval_function(self.label, self.pred)
 
