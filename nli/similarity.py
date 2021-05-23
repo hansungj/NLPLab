bfcd92d34e5e942e703da6801675d76875311d87
Jun <43443895+hansungj@users.noreply.github.com>
2021-05-23 16:08:54 +0200

    Update similarity.py
    fixed spaces

--- nli/similarity.py
+++ nli/similarity.py
@@ -6,9 +6,7 @@ def levenshtein(x,y):
 	'''
 	levenshtein distance 
 	'''
-
 	dp = [[0]*(len(y)+1) for _ in range(len(x)+1)] 
-
 	for i in range(1, len(x)+1):
 		for j in range(1,len(y)+1):
 
@@ -16,7 +14,6 @@ def levenshtein(x,y):
 				dp[i][j] = min(dp[i-1][j-1], dp[i-1][j]+1, dp[i][j-1]+1) 
 			else:
 				dp[i][j] = min(dp[i-1][j-1]+1, dp[i-1][j]+1, dp[i][j-1]+1) 
-
 	return dp[-1][-1]
 
 
