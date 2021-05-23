50d91db9d95fb8bd092fea3ff2e28209b9d6687d
Jun <43443895+hansungj@users.noreply.github.com>
2021-05-23 16:08:22 +0200

    Create build_vocab.py

---
+++ scripts/build_vocab.py
@@ -0,0 +1,90 @@
+'''
+
+build_vocab.py
+
+here we can build either 
+
+1. regular vocabulary 
+2. byte pair encoding vocabulary 
+
+'''
+import logging
+import os
+import numpy as np
+import argparse
+import pickle
+import time
+import json
+
+
+from nli.preprocess import token_to_idx, idx_to_token, frequency_count
+from nli.utils import open_tsv_file
+
+parser = argparse.ArgumentParser()
+logger = logging.getLogger(__name__)
+
+#directory for data/train/val 
+parser.add_argument('--data_path', default='data/alphanli/tsv/train.tsv', type=str, help='data to build the vocabulary on')
+parser.add_argument('--out_dir', default='data/alphanli/tsv', type=str, help='output directory for vocab json')
+parser.add_argument('--vocab_type', default='regular', type=str, help='build regular or bpe')
+parser.add_argument('--min_occurence', default=1, type=str, help='minimum occurence of a token to include in a vocabulary')
+parser.add_arguent('--vocabulary_size', default = None, type=int)
+
+
+def main(args):
+
+	if args.vocab_type == 'regular':
+		data = open_tsv_file(args.data_path)
+		freq_count = frequency_count(dataset)
+
+		#filter by vocabulary size
+		logger.info('Unfiltered vocabulary length: {}'.format(len(freq_count)))
+	
+
+		#min occurence 
+		if min_occurence is not None:
+			for token, count in freq_count.items():
+				if count < min_occurence:
+					del freq_count[key]
+
+		if vocbulary_size is not None:
+			freq_count = sorted(((k,v) in k,v in freq_count.items()), reverse=True, key = lambda x: x[1])[:vocbulary_size]
+			freq_count = {k:v for k,v in freq_count}
+
+		token2idx = token_to_idx(freq_count, 
+								delimiters = r'\s+', 
+								start_symbol = True, 
+								end_symbol = True, 
+								null_symbol = True,
+								split_symbol = True)
+
+		idx2token = idx_to_token(token2idx)
+
+		logger.info('filtered vocabulary length: {}'.format(len(token2idx)))
+
+	elif args.vocab_type == 'bpe':
+		'''
+		build vocabulary here
+
+		'''
+		NotImplementedError 
+
+	#finally save
+	vocab = {
+	'token2idx':
+	'idx2token':
+	}
+
+	with open(os.path.join(args.out_path, 'vocab.json'), 'w') as f:
+		json.dump(vocab, f, indent=4)
+
+
+
+if __name__ == '__main__':
+	args = parser.parse_args()
+	#If you set the log level to INFO, it will include INFO, WARNING, ERROR, and CRITICAL messages
+	logging.basicConfig(
+		level=logging.INFO,
+		format='%(name)s: %(asctime)s: %(message)s'
+		)
+	main(args)
