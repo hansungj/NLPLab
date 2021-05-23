696e948e0c2d2926aa1dbe7e7338c1ad369f31a0
Jun <43443895+hansungj@users.noreply.github.com>
2021-05-23 16:08:44 +0200

    Update utils.py

--- nli/utils.py
+++ nli/utils.py
@@ -4,37 +4,24 @@ import argparse
 import json 
 from collections import defaultdict 
 
-
 def open_label_file(path):
-
 	if path:
 		with open(path, 'r') as f:
 			labels = [int(l) for l in f.read().splitlines()]
-	
-	
 		return labels 
-
 	raise ValueError('Must give label path')
 
-
-
 def open_json_file(path):
-
 	if path:
 		with open(path, 'r') as f:
 		 	data = json.load(path)
-
 		return data 
-
 	raise ValueError('Must give json data path')
 
-
 def open_tsv_file(path, dic=False):
-
 	if path:
 		with open(path, 'r') as f:
 			data = [l.split('\t') for l in f.read().splitlines()]
-
 		if dic:
 			res = defaultdict(list)
 			for line in data:
@@ -44,10 +31,6 @@ def open_tsv_file(path, dic=False):
 				res['hyp1'].append(line[3])
 				res['hyp2'].append(line[4])
 				res['label'].append(0 if int(line[5])==1 else 1)
-
 			return res
-
 		return data
-
-
 	raise ValueError('Must give tsv data path')
