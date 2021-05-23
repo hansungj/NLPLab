612874db2902dfbde75bcd1072232db829b6c920
Jun <43443895+hansungj@users.noreply.github.com>
2021-05-23 16:13:19 +0200

    Update embedding.py
    method for loading embedding matrix with Glove

--- nli/embedding.py
+++ nli/embedding.py
@@ -1,3 +1,22 @@
 #embedding.py 
 
+import torch
+import torch.nn as nn
+import gensim
+import gensim.downloader as api
+
+def build_embedding_glove(vocab, glove_name):
+
+	glove = api.load(glove_name)
+	N = len(vocab['token2idx'])
+	H = glove.vector_size
+	embedding = nn.Embedding(N, H)
+
+	#initialize embedding matrix
+	for token, idx in vocab['token2idx'].items():
+		# we initalize the embedding matrix 
+		if token in glove:
+			embedding.weight[idx, :] = glove[token]
+
+	return embedding
 
