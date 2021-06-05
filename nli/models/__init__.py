422e0ffa0fc31543a8360777ae0918980c165f78
Jun <43443895+hansungj@users.noreply.github.com>
2021-06-05 21:58:33 +0900

    Update __init__.py

--- nli/models/__init__.py
+++ nli/models/__init__.py
@@ -2,4 +2,5 @@
 
 from nli.models.BoW import MaxEnt, LogisticRegression, BagOfWords
 from nli.models.StaticEmb import StaticEmbeddingMixture, StaticEmbeddingRNN, StaticEmbeddingCNN
+from nli.models.Transformers import PretrainedTransformerCLS, PretrainedTransformerPooling
 
