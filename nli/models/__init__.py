f6caa911051b74eeb2e9d29b3fa0a46fbab3f66f
overnastuhed <58928692+overnastuhed@users.noreply.github.com>
2021-06-06 09:00:05 +0300

    Update cosine

--- nli/models/__init__.py
+++ nli/models/__init__.py
@@ -1,5 +1,5 @@
 # initialization for models 
 
-from nli.models.BoW import MaxEnt, LogisticRegression, BagOfWords
+from nli.models.BoW import MaxEnt, LogisticRegression, BagOfWords, Perceptron
 from nli.models.StaticEmb import StaticEmbeddingMixture, StaticEmbeddingRNN, StaticEmbeddingCNN
 
