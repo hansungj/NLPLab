dc0467aa47479668859bbcce0fe0555cfaf188db
SUNGJUN HAN <sungjun.han@daimler.com>
2021-06-18 23:14:14 +0900

    updated data file

--- nli/pretrain/data.py
+++ nli/pretrain/data.py
@@ -9,6 +9,7 @@ will host data loading objects for
 import json
 import numpy as np
 import re
+import random
 
 import torch
 import torch.utils.data import DataLoader, Dataset
@@ -24,23 +25,29 @@ class BookCorpusLmLoader(DataLoader):
         tokenizer = kwargs.pop('tokenizer')
         max_context_length = kwargs.pop('max_context_length')
         max_target_lenfgth = kwargs.pop('max_target_length')
-        distributed = kwargs.pop('distributed', False)
+        left_context = kwargs.pop('left_context', True)
+        right_context = kwargs.pop('right_context', False )
+        context_window = kwargs.pop('context_window', 1)
 
         # define dataset here 
         dataset = BookCorpusLmDataset(
             data, 
             tokenizer,
+            left_context,
+            right_context,
             max_context_length,
-            max_target_length)
+            max_target_length,
+            context_window)
 
         # if distributed use Sampler and set shuffle to False 
+        distributed = kwargs.pop('distributed', False)
         if distributed:
             shuffle=kwargs['shuffle']# we need to define shuffle in terms of our sampler
             kwargs['shuffle'] = False  # set shuffle to false for the dataloader as handled by sampler
             sampler=DistributedSampler(dataset=dataset, shuffle=shuffle)
             kwargs['sampler'] = sampler 
         
-        kwargs['collate_fn'] = collate_fn_BookCorpusLM
+        kwargs['collate_fn'] = collate_fn_bookcorpus_lm
         super(BookCorpusLmLoader, self).__init__(self, dataset, **kwargs)
 
 
@@ -52,7 +59,7 @@ def merge(sequences):
         padded_seqs[i, :end] = seq[:end]
     return padded_seqs, lengths
 
-def collate_fn_BookCorpusLM(data):
+def collate_fn_bookcorpus_lm(data):
 
     batch = {}
     for key in data[0].keys():
@@ -78,32 +85,58 @@ class BookCorpusLmDataset(Dataset):
     def __init__(self, 
         data,
         tokenizer,
-        max_context_length,
-        max_target_length):
+        left_context=True,
+        right_context=False,
+        max_context_length=128,
+        max_target_length=128,
+        context_window=1):
         super(BookCorpusLmLoader, self).__init__()
 
         self.data = data
         self.tokenizer = tokenizer
+        self.left_context = left_context
+        self.right_context = right_context
         self.max_context_length = max_context_length
         self.max_target_length = max_target_length
+        self.context_window = context_window
+
+        #left context and right context cannot be both False 
+        assert(left_context or right_context)
 
     def __len__(self):
-        return len(self.data['text'])
+        # -1 because we cannot use the first sample
+        return len(self.data['text'])-2 
     
     def __getitem__(self, idx):
-        text = self.data['text'][idx]# assume it is in this form   
-        control = text[:-1] # this can be a list or a single element 
-        target =text[-1]  
+        '''
+        using the huggingface dataset object 
+        we will dynamically prepare the samples
+
+        we will create the control from the previous/next context window through random sampling 
+        '''
+        
+        if idx == 0 and self.left_context:
+            idx += 1  # we cannot use the first sample because its previous context does not exist 
+        
+        if idx == len(self.data['text'])-1 and self.right_context:
+            idx -= 1 # we cannot use the last sample because its subsequent context does not exist 
+
+        target = self.data['text'][idx] 
+
+        control = []
+        #random sample 
+        if self.left_context:
+            l_idx=random.randint(max(0,idx-self.context_window, idx-1))
+            l_context = 'antecedent : ' + self.data['text'][l_idx]
+            control.append(l_context)
+        
+        if self.right_context:
+            r_idx=random.randint(idx+1, min(len(self.data['text'])-1,idx+self.context_window))
+            r_context = ' subsequent : + self.data['text'][r_idx]
+            control.append(r_context)
 
         input_ids = []
         target_ids = []
-
-        #make the control code explictit -- this way we can clearly distingjish between the antecedent/subsequent if we choose to do so 
-        if len(text) > 2: 
-            control = 'antecedent : ' + control[0] + ' subsequent : ' + control[1:]
-        else:
-            control = 'antecedent : ' + control
-
         tokens = self.tokenizer.tokenize(control)
 		tokens.insert(0, self.tokenizer.cls_token)
 		tokens.append(self.tokenizer.sep_token)
