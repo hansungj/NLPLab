fd9613998402b5a236e94f9d3552d0e6d3b3a959
SUNGJUN HAN <sungjun.han@daimler.com>
2021-06-21 16:57:41 +0900

    fixed gpt finetune

--- nli/models/Transformers.py
+++ nli/models/Transformers.py
@@ -40,30 +40,30 @@ class PretrainedTransformerCLS(nn.Module):
 class PretrainedTransformerPooling(nn.Module):
 
 	def __init__(self, model_name, dropout=0.2):
-
+		super().__init__()
 		self.model = AutoModel.from_pretrained(model_name)
 		self.config = AutoConfig.from_pretrained(model_name)
 
 		hidden_size = self.config.hidden_size
 		self.classifier = nn.Sequential(
-			nn.LayerNorm(),
+			nn.LayerNorm(hidden_size),
 			nn.Dropout(dropout),
 			nn.Linear(hidden_size, hidden_size),
 			nn.GELU(),
-			nn.LayerNorm(),
+			nn.LayerNorm(hidden_size),
 			nn.Dropout(dropout),
 			nn.Linear(hidden_size,1)
 		)
 		self.loss_fn = nn.BCEWithLogitsLoss()
 
-	def forward(self, x, y=None):
+	def forward(self, input_ids, segment_ids, masks, y=None):
 		'''
 		we pool the representations and use this for prediction 
 		'''
 		
 		output = self.model(input_ids = input_ids, attention_mask = masks, token_type_ids = segment_ids)
-		sentence_emb = torch.max(output, dim =-1)
-		logits = self.classifier(logits)
+		sentence_emb = torch.max(output.last_hidden_state, dim =1)[0]
+		logits = self.classifier(sentence_emb)
 
 		if y is not None:
 			loss = self.loss_fn(logits.view(-1),y.view(-1))
