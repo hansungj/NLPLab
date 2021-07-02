6e4fe35f780eb69afd30422e72316b01a5c0f0ff
overnastuhed <58928692+overnastuhed@users.noreply.github.com>
2021-07-02 02:10:06 +0200

    Update train file

--- scripts/pretrain_mlm.py
+++ scripts/pretrain_mlm.py
@@ -5,7 +5,7 @@ Description: for pretraining BERT
 # tutorial by James Briggs used:
 # https://towardsdatascience.com/masked-language-modelling-with-bert-7d49793e5d2c
 
-from transformers import BertConfig, BertForMaskedLM
+from transformers import BertTokenizer
 from transformers import AdamW
 from datasets import load_dataset
 
@@ -13,18 +13,23 @@ import numpy as np
 import argparse
 from tqdm import tqdm
 import logging
+import time
 import os
+import torch
 
 
-from nli.pretrain-mlm.BERT.bert-mlm import BertMLM
+
+from nli.pretrain_mlm.BERT.bert_mlm import BertMLM
+from nli.pretrain_mlm.BERT.data import MLM_Dataloader
 
 parser = argparse.ArgumentParser()
 logger = logging.getLogger(__name__)
 
-argparse.add_argument('--corpus_name', default='bookcorpus', type=str)
+parser.add_argument('--corpus_name', default='bookcorpus', type=str)
 
 parser.add_argument('--pretrained_name', default='bert-base-uncased', type=str, help='can be used to initialize a pretrained model from huggingface')
 parser.add_argument('--max_samples_per_epoch', default=1000, type=int, help='Number of samples per epoch')
+parser.add_argument('--epochs', default=100, type=int, help='Number of epochs')
 
 parser.add_argument('--use_cuda', default=False, type=bool, help = 'activate to use cuda')
 
@@ -32,27 +37,25 @@ parser.add_argument('--use_cuda', default=False, type=bool, help = 'activate to 
 parser.add_argument('--batch_size', default=128, type=int)
 parser.add_argument('--num_workers', default=0, type=int)
 parser.add_argument('--shuffle', default=True, type=bool)
+parser.add_argument('--masking_prob', default=0.15, type=float)
 
-argparse.add_argument('--save_model_to', default='nli\pretrain-mlm\BERT\pretrained_model', type=str)
+parser.add_argument('--save_model_to', default='nli\pretrain-mlm\BERT\pretrained_model', type=str)
 
 def main(args):
-
-	utils.set_seed(args.seed)
 	
 	tokenizer = BertTokenizer.from_pretrained(args.pretrained_name)
 	
-	dataset_init = {'dataset': args.corpus_name,
-					'max_samples': args.max_samples_per_epoch,
-					'tokenizer' : tokenizer}
+	dataloader_kwargs = {'data':args.corpus_name,
+						'tokenizer':tokenizer,
+						'batch_size':args.batch_size,
+						'shuffle':args.shuffle,
+						'num_workers':args.num_workers,
+						'masking_prob':args.masking_prob
+						}
 	
-	train_dataset = MLM_Dataset(**dataset_init)
+	train_loader = MLM_Dataloader(**dataloader_kwargs)
 	
-	train_loader = load_dataloader_transformer(
-						train_dataset, 
-						args.batch_size, 
-						shuffle=args.shuffle, 
-						drop_last = True, 
-						num_workers = args.num_workers)
+	#val_loader = 
 	
 	model = BertMLM(args.pretrained_name)
 	
@@ -68,7 +71,7 @@ def main(args):
 	model.to(device)
 	model.train()
 	
-	for epoch in range(epochs):
+	for epoch in range(args.epochs):
 
 		loop = tqdm(train_loader, leave=True)
 		
@@ -77,14 +80,12 @@ def main(args):
 			optim.zero_grad()
 
 			input_ids = batch['input_ids'].to(device)
-			attention_mask = batch['attention_mask'].to(device)
-			labels = batch['labels'].to(device)
+			masks = batch['masks'].to(device)
+			labels = batch['output_ids'].to(device)
+			segment_ids = batch['segment_ids'].to(device)
 
-			outputs = model(input_ids, attention_mask=attention_mask,
-                        labels=labels)
+			logits, loss = model(input_ids, segment_ids = segment_ids, attention_mask=masks, labels=labels)
 			
-			loss = outputs.loss
-
 			loss.backward()
 
 			optim.step()
@@ -94,4 +95,12 @@ def main(args):
 	
 	model.save_pretrained(args.save_model_to)
 	
-	
+
+if __name__ == '__main__':
+	args = parser.parse_args()
+	#If you set the log level to INFO, it will include INFO, WARNING, ERROR, and CRITICAL messages
+	logging.basicConfig(
+		level=logging.INFO,
+		format='%(name)s: %(asctime)s: %(message)s'
+		)
+	main(args)	
