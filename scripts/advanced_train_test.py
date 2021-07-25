c836ecc9fb5b245b9d719c127d01f0fcdacf4f81
overnastuhed <58928692+overnastuhed@users.noreply.github.com>
2021-07-25 16:44:59 +0200

    Update advanced_train_test.py

--- scripts/advanced_train_test.py
+++ scripts/advanced_train_test.py
@@ -573,9 +573,9 @@ def main(args):
 
 		# initialize tokenizer
 		if 'BERTmlm' in args.pretrained_name:
-				tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-			else:
-				tokenizer = BertTokenizer.from_pretrained(args.pretrained_name)
+			tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+		else:
+			tokenizer = BertTokenizer.from_pretrained(args.pretrained_name)
 
 		#ensure that the tokenizer has all the functional tokens
 		if tokenizer.cls_token == None:
