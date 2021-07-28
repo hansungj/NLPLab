3923a5a5977d739eb49862560c1e06f52444410b
overnastuhed <58928692+overnastuhed@users.noreply.github.com>
2021-07-28 16:18:42 +0200

    Renaming BERTmlm

--- nli/pretrain_mlm/__init__.py
+++ nli/pretrain_mlm/__init__.py
@@ -1,5 +1,5 @@
 # initialization for BERT for masked language modelling 
 
-from nli.pretrain_mlm.bert_mlm import BertMLM
+from nli.models.BERT import BertMLM
 from nli.pretrain_mlm.dataloader import MLM_Dataloader
 
