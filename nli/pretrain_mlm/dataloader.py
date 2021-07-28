c1d501398911aedcd5e777e7eb920cda94c99267
overnastuhed <58928692+overnastuhed@users.noreply.github.com>
2021-07-28 15:49:41 +0200

    Update dataloader.py

--- nli/pretrain_mlm/dataloader.py
+++ nli/pretrain_mlm/dataloader.py
@@ -7,6 +7,8 @@ import json
 import numpy as np
 import re
 from datasets import load_dataset
+from transformers import BertTokenizer
+import random
 
 
 import torch
@@ -47,10 +49,10 @@ class MLM_Dataloader(DataLoader):
 		for key in batch[0].keys():
 			item[key] = [d[key] for d in batch]
 
-		input_ids = self.padding(item['input_ids'])
-		masks = self.padding(item['masks'])
-		segment_ids = self.padding(item['segment_ids'])
-		target_ids = self.padding(item['target_ids'])
+		input_ids = self.padding(item['input_ids'], 0)
+		masks = self.padding(item['masks'], 0)
+		segment_ids = self.padding(item['segment_ids'], 0)
+		target_ids = self.padding(item['target_ids'], -100)
 
 		d = {}
 		d['input_ids'] = input_ids
@@ -60,8 +62,8 @@ class MLM_Dataloader(DataLoader):
 		d['segment_ids'] = segment_ids
 		return d
 
-	def padding(self, datalist):
-		pad_token_id = self.tokenizer.convert_tokens_to_ids(self.tokenizer.pad_token)
+	def padding(self, datalist, pad_token_id):
+		#pad_token_id = self.tokenizer.convert_tokens_to_ids(self.tokenizer.pad_token)
 		max_len = max([len(item) for item in datalist])
 		padded_datalist = torch.zeros((len(datalist), max_len)).long()
 		for i in range(len(datalist)):
@@ -127,11 +129,11 @@ class MLM_Dataset(Dataset):
 			output_sent = self.data['text'][idx]
 
 		if self.context_left and self.context_right:
-			input_sent = left_input_sent + ' ' + right_input_sent
+			input_sent = (left_input_sent, right_input_sent)
 		elif self.context_left:
-			input_sent = left_input_sent
+			input_sent = (left_input_sent,)
 		elif self.context_right:
-			input_sent = right_input_sent
+			input_sent = (right_input_sent,)
 		else:
 			raise ValueError('Both --context_left and --context_right cannot be set to False')
 
@@ -140,7 +142,8 @@ class MLM_Dataset(Dataset):
 		item = {}
 		item['input_ids'] = torch.tensor(input)
 		item['masks'] = torch.tensor(masking)
-		item['reference'] = '[CLS] ' + input_sent + ' [SEP] ' + output_sent + ' [EOS]'
+		context = ' [SEP] '.join(list(input_sent))
+		item['reference'] = '[CLS] ' + context + ' [SEP] ' + output_sent + ' [EOS]'
 		item['target_ids'] = torch.tensor(target)
 		item['segment_ids'] = torch.tensor(segment_ids)
 		return item
@@ -164,49 +167,125 @@ class MLM_Dataset(Dataset):
 		target = []
 
 		input.append(self.tokenizer.cls_token)
-		input_tokenized = self.tokenizer.tokenize(input_sent)
-		if len(input_tokenized) < self.max_context_length:
-				input += input_tokenized
-		else:
-			input += input_tokenized[:self.max_context_length]
-
-		input.append(self.tokenizer.sep_token)
-
-		random_nums = torch.rand(len(input))
-
-		#mask a token with prob of 15%, excluding CLS tokens, SEP tokens
-		#generating a masking filter 
-		masking = (random_nums < masking_prob) * (input != self.tokenizer.cls_token) \
-			* (input != self.tokenizer.sep_token) # * (input!= self.tokenizer.pad_token)
-
-		# which tokens have to be masked according to the masking filter
-		tokens_to_mask = []
-		for i in range(len(masking)):
-			if masking[i] == True:
-					tokens_to_mask.append(i)
-
-		for token in tokens_to_mask:
-			input[token] = self.tokenizer.mask_token
 		
+		for input_sent_ in input_sent:
+			input_tokenized = self.tokenizer.tokenize(input_sent_)
+			if len(input_tokenized) < self.max_context_length:
+				input += input_tokenized
+			else:
+				input += input_tokenized[:self.max_context_length]
+
+			input.append(self.tokenizer.sep_token)
+
 		input = self.tokenizer.convert_tokens_to_ids(input)
 
 		target_tokenized = self.tokenizer.tokenize(output_sent)
 		
 		if len(target_tokenized) < self.max_target_length:
-				target = target_tokenized
+			target = target_tokenized
 		else:
 			target = target_tokenized[:self.max_target_length]
 		
-		target = self.tokenizer.convert_tokens_to_ids(target)
+		target.append(self.tokenizer.eos_token)
+		
+		#deprecated: mask a token with prob of 15%,
+		#random_nums = torch.rand(len(target))
+		##mask a token with prob of 15%, excluding CLS tokens, SEP tokens
+		##generating a masking filter 
+		#masking = (random_nums < masking_prob) * (target != self.tokenizer.cls_token) \
+			#* (target != self.tokenizer.sep_token)  * (target != self.tokenizer.eos_token)
+		## which tokens have to be masked according to the masking filter
+		#tokens_to_mask = []
+		#for i in range(len(masking)):
+			#if masking[i] == True:
+					#tokens_to_mask.append(i)
+
+		target_unmasked = [self.ignore_index]*len(target)
+		target_masked = self.tokenizer.convert_tokens_to_ids(target)
+		
+		indeces = [t for t in range(len(target)-1)] # -1 so that we don't mask the EOS token
+		#print('indeces = {}'.format(indeces))
+		
+		num_to_mask = round(len(target)*masking_prob)
+		#print('num_to_mask = {}'.format(num_to_mask))
+		
+		tokens_to_mask = random.sample(indeces, k=num_to_mask)
+		#print('tokens_to_mask = {}'.format(tokens_to_mask))
+		
+		randoms = list(torch.rand(len(tokens_to_mask)))
+		#print('randoms = {}'.format(randoms))
+				
+		mask_token = self.tokenizer.convert_tokens_to_ids(self.tokenizer.mask_token)
+		
+		for idx in tokens_to_mask:
+		
+			#print('idx = {}'.format(idx))
+			target_unmasked[idx] = target_masked[idx]
+			prob = randoms.pop()
+			#print('prob = {}'.format(prob))
+			
+			if prob <= 0.8:
+				target_masked[idx] = mask_token
+				
+			elif (prob > 0.8) and (prob <= 0.9):
+				random_token = random.randint(0, len(self.tokenizer)-1)
+				#print('random_token = {}'.format(random_token))
+				#print('target_masked[idx]  = {}'.format(target_masked[idx]))
+				target_masked[idx] = random_token
+				
+			#elif prob >0.9:
+				#target_unmasked[idx] is unchanged
 		
 		segment_ids = [0] * len(input) + [1] * len(target)
 
 		len_input = len(input)
-		input += target
+		input += target_masked
 		
-		post_masking = torch.tensor([False]*len(target))
+		pre_masking = torch.tensor([False]*len_input)
 
-		target = [self.ignore_index] * len_input + target
+		target = [self.ignore_index] * len_input + target_unmasked
 		
-		masking_fin = torch.cat((masking, post_masking), 0)
+		masking_fin = [1]*len(input)
 		return input, target, masking_fin, segment_ids
+
+if __name__ == '__main__':
+	tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+	#tokenizer = BertTokenizer.from_pretrained(args.pretrained_name, cache_dir = '../huggingface') #for running on the ims server
+
+	#ensuring that all functional tokens are added to the default tokenizer
+	if tokenizer.cls_token == None:
+		tokenizer.add_special_tokens({'cls_token': '<CLS>'})
+	if tokenizer.sep_token == None:
+		tokenizer.add_special_tokens({'sep_token': '<SEP>'})
+	if tokenizer.eos_token == None:
+		tokenizer.add_special_tokens({'eos_token': '<EOS>'})
+	
+	print(tokenizer.convert_tokens_to_ids(tokenizer.eos_token))
+	print(tokenizer.convert_tokens_to_ids(tokenizer.mask_token))
+	print(tokenizer.convert_tokens_to_ids(tokenizer.pad_token))
+	print(tokenizer.sep_token)
+	print(tokenizer.mask_token)
+	
+	dataset_preload = load_dataset('bookcorpus')
+	
+	dataloader_kwargs = {'data':'bookcorpus',
+						'tokenizer':tokenizer,
+						'batch_size':1,
+						'shuffle':True,
+						'num_workers':0,
+						'masking_prob':0.30,
+						'max_context_length':128,
+						'max_target_length':92,
+						'context_left':True,
+						'context_right':True,
+						'number_of_samples':4
+						}
+
+	train_loader = MLM_Dataloader(**dataloader_kwargs)
+	
+	for batch in enumerate(train_loader):
+		print(batch)
+		print(batch[1]['input_ids'].size())
+		print(batch[1]['masks'].size())
+		print(batch[1]['target_ids'].size())
+		print(batch[1]['segment_ids'].size())
