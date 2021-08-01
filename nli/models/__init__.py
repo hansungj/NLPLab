9f3fe045b9e7be54fa914e7f0713bfc8bb665fa4
Jun <43443895+hansungj@users.noreply.github.com>
2021-08-01 12:20:20 +0900

    Update __init__.py

--- nli/models/__init__.py
+++ nli/models/__init__.py
@@ -3,3 +3,4 @@ from nli.models.BoW import MaxEnt, LogisticRegression, BagOfWords, Perceptron
 from nli.models.StaticEmb import StaticEmbeddingMixture, StaticEmbeddingRNN, StaticEmbeddingCNN
 from nli.models.Transformers import PretrainedTransformerCLS, PretrainedTransformerPooling
 from nli.models.GPT2 import PretrainedDecoderTransformerDual, PretrainedDecoderTransformerCLS, PretrainedDecoderTransformerDualSingleClassifier
+from nli.models.BERT import BertMLM
